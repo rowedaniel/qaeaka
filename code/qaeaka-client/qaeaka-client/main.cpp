3597528fe9c46190b1dad3440c3a152209203220
Daniel Rowe <rowedanielp@gmail.com>
2021-05-20 16:13:39 -0700

    added sfml lib files

--- code/qaeaka-client/qaeaka-client/main.cpp
+++ code/qaeaka-client/qaeaka-client/main.cpp
@@ -4,12 +4,12 @@ int main(int argc, char** argv[])
 {
 	std::cout << "Starting client." << std::endl;
 
-	sf::TcpSocket socket;
-	sf::Socket::Status status = socket.connect("127.0.0.1", 53000);
-	if (status != sf::Socket::Done)
-	{
-		// error...
-	}
+	sf::IpAddress server("127.0.0.1");
+	int port = 53000;
+
+	sf::UdpSocket socket;
+
+	// if(socket.send(msg, sizeof(msg), server, port) != Socket::Done) { /* error */ }
 
 	return 0;
 }
--- code/qaeaka-server/qaeaka-server/main.cpp
+++ code/qaeaka-server/qaeaka-server/main.cpp
@@ -4,14 +4,29 @@ int main(int argc, char** argv[])
 {
 	std::cout << "Starting server." << std::endl;
 
-	// Create a socket to receive a message from anyone
-    sf::UdpSocket socket;
+	int port = 53000;
 
-    // Listen to messages on port 53000
-    if (socket.bind(53000) != sf::Socket::Done)
+	// Create a socket to receive a message from anyone
+	sf::UdpSocket socket;
+
+	// Listen to messages on port 53000
+	if (socket.bind(53000) != sf::Socket::Done)
 	{
 		std::cout << "Error starting server." << std::endl;
+		return;
 	}
+
+
+	/*
+	char in[size]
+	std::size_t received;
+	sf::IpAddress
+	unsigned short senderPort
+	if (socket.receive(in, sizeof(in), received, sender, senderPort) != sf::Socket::Done) 
+	{ 
+		// error 
+	}
+	*/
 
 
 
